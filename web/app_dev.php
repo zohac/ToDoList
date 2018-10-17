340241d037025cfb73a86279e912130244a8144c
zohac <fenrir0680@gmail.com>
2018-10-17 20:40:41 +0200

    build(core): codacy/coverage installation

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,27 +8,6 @@ use Symfony\Component\Debug\Debug;
 // for more information
 //umask(0000);
 
-/**
- * BlackFire extension
- */
-// If the header is set
-if (isset($_SERVER['HTTP_BLACKFIRETRIGGER'])) {
-    $config = new \Blackfire\ClientConfiguration();
-    $config->setClientId('852814fc-548f-4216-bc37-03d5aab5662f');
-    $config->setClientToken('c54644f37928f624f157280c2e1ec5353a11044c81022b4bfdcee1dc9284cc00');
-
-    // let's create a client
-    $blackfire = new \Blackfire\Client($config);
-    // then start the probe
-    $probe = $blackfire->createProbe();
-
-    // When runtime shuts down, let's finish the profiling session
-    register_shutdown_function(function () use ($blackfire, $probe) {
-        // See the PHP SDK documentation for using the $profile object
-        $profile = $blackfire->endProbe($probe);
-    });
-}
-
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
