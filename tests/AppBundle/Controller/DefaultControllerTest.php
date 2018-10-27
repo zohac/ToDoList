d62fa98a79b38e9c011da84f77fc934a05dd8142
zohac <fenrir0680@gmail.com>
2018-10-27 12:47:47 +0200

    test(controller): default controller

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -5,14 +5,41 @@ namespace Tests\AppBundle\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * Test the default controller.
+ */
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    /**
+     * Test the route with no authentication.
+     */
+    public function testWithoutAuthenticationIndex()
     {
+        // Create a client
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        // Request the route
+        $client->request('GET', '/');
 
+        // Test assert
         $this->assertEquals(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Test the route with an authenticated user.
+     */
+    public function testWithAuthenticationIndex()
+    {
+        // Create an authenticated client
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'user1',
+            'PHP_AUTH_PW' => 'Aa@123',
+        ]);
+
+        // Request the route
+        $client->request('GET', '/');
+
+        // Test assert
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
     }
 }
